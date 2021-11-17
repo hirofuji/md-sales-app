8f639828a9fe795f7a9ba53053da94c5acaad616
hiroaki fujimori <fujimori@fourtune.co.jp>
2021-11-17 18:07:22 +0900

    mod init.sh

--- init.sh
+++ init.sh
@@ -13,22 +13,4 @@ docker run --rm \
     -v "$(pwd)":/opt \
     -w /opt \
     laravelsail/php80-composer:latest \
-    bash -c "php artisan sail:install"
-
-CYAN='\033[0;36m'
-LIGHT_CYAN='\033[1;36m'
-WHITE='\033[1;37m'
-NC='\033[0m'
-
-echo ""
-
-if sudo -n true 2>/dev/null; then
-    sudo chown -R $USER: .
-    echo -e "${WHITE}Get started with:${NC} ./vendor/bin/sail up"
-else
-    echo -e "${WHITE}Please provide your password so we can make some final adjustments to your application's permissions.${NC}"
-    echo ""
-    sudo chown -R $USER: .
-    echo ""
-    echo -e "${WHITE}Thank you! We hope you build something incredible. Dive in with:${NC} ./vendor/bin/sail up"
-fi
+    bash -c "composer install"
